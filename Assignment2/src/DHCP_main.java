c10cd114da2c900a28141f927005e4d8fbfb87d2
liesbollens <liesbollens@10.43.78.86>
2016-03-09 08:14:23 +0100

    addded arguments to main function call

--- Assignment2/src/DHCP_main.java
+++ Assignment2/src/DHCP_main.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
-import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 
 public class DHCP_main {
 	public static void main(String args[]) throws Exception {
-		DHCP_Client client = new DHCP_Client();
+		DHCP_Client client = new DHCP_Client("10.33.14.246", 1234);
 		byte[] packet = client.DHCP_discover();
 		client.send_packet(packet);
 		

3cba87f98bc885a68820477237a5e24f77f19595
quinten991 <quinten.lauwers1@student.kuleuven.be>
2016-03-16 16:17:04 +0100

    config file for server

--- Assignment2/src/DHCP_Server.java
+++ Assignment2/src/DHCP_Server.java
@@ -1,29 +1,62 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-
-import com.sun.security.auth.login.ConfigFile;
+import java.net.URL;
+import java.util.Properties;
 
 
 public class DHCP_Server {
-	
+
 	private int port; 
-	
+	private int max_connections;
+
 	public DHCP_Server(int port, String config){
 		this.port = port;
-		File configFile = null; 
-		try {
-			configFile = new File(config);
-		} catch( NullPointerException e ){
-			return; 
-		}
-		BufferedReader bufferedReader = new BufferedReader(ConfigFile);
-		
-		
-		
-		
+		config = convert_config_path(config);
+		parse_config_file(config);
+		System.out.println(max_connections);
+
+
+
+
 	}
-	
+
+	private String convert_config_path(String config) {
+		URL config_url = getClass().getResource(config);
+		if (config_url != null) {
+			return config_url.getPath();
+		}
+		return config;
+	}
+
+	private void parse_config_file(String config_path) {
+		boolean file_load_succesfull = true;
+		
+		Properties prop = new Properties();
+		try {
+			InputStream input = new FileInputStream(config_path);
+			prop.load(input);
+
+		} catch( Exception e ){
+			System.out.println(e);
+			file_load_succesfull = false; 
+		}
+		
+		String max_conn = null;
+		
+		if (file_load_succesfull) {
+			max_conn = prop.getProperty("max_connections");
+		}
+		
+		if (max_conn != null) {
+			this.max_connections = Integer.parseInt(max_conn);
+		}
+		else {
+			this.max_connections = 3;
+		}
+	}
 }
--- Assignment2/src/DHCP_main.java
+++ Assignment2/src/DHCP_main.java
@@ -1,37 +1,41 @@
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.net.URL;
 import java.util.Enumeration;
 
 
 
 public class DHCP_main {
 	public static void main(String args[]) throws Exception {
+//		
+//		// get the mac adddress from the computer 
+//		byte[] macAddress = null;
+//		Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+//        while(networkInterfaces.hasMoreElements())
+//        {
+//            NetworkInterface network1 = networkInterfaces.nextElement();
+//            byte[] mac = network1.getHardwareAddress();
+//            if(mac != null)
+//            {
+//                macAddress = mac;
+//            }
+//        }
+//        if (macAddress == null) {
+//    		InetAddress ip = InetAddress.getLocalHost();
+//    		System.out.println(ip);
+//    		NetworkInterface network = NetworkInterface.getByInetAddress(ip);
+//    			System.out.println(network);
+//		macAddress = network.getHardwareAddress(); 
+//		
+//        }
+//        
+//        System.out.println(macAddress);
+//		
+//		DHCP_Client client = new DHCP_Client("192.168.1.1", 68, 67, macAddress);
+//
+//		System.out.println("Received IP-address: " + client.execute_DHCP().getHostAddress());		
 		
-		// get the mac adddress from the computer 
-		byte[] macAddress = null;
-		Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
-        while(networkInterfaces.hasMoreElements())
-        {
-            NetworkInterface network1 = networkInterfaces.nextElement();
-            byte[] mac = network1.getHardwareAddress();
-            if(mac != null)
-            {
-                macAddress = mac;
-            }
-        }
-        if (macAddress == null) {
-    		InetAddress ip = InetAddress.getLocalHost();
-    		System.out.println(ip);
-    		NetworkInterface network = NetworkInterface.getByInetAddress(ip);
-    			System.out.println(network);
-		macAddress = network.getHardwareAddress(); 
-		
-        }
-        
-        System.out.println(macAddress);
-		
-		DHCP_Client client = new DHCP_Client("192.168.1.1", 68, 67, macAddress);
-
-		System.out.println("Received IP-address: " + client.execute_DHCP().getHostAddress());		
+		new DHCP_Server(500, "server.conf");
+	
 	}
 }

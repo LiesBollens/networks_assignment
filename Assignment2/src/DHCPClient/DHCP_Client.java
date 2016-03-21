0ae2e746025510f3904071157944c9ee6f068e50
quinten991 <quinten.lauwers1@student.kuleuven.be>
2016-03-21 16:04:44 +0100

    Added quick and dirty lease time renual on the client

--- Assignment2/src/DHCPClient/DHCP_Client.java
+++ Assignment2/src/DHCPClient/DHCP_Client.java
@@ -14,7 +14,7 @@ import DHCPShared.DHCP_package;
 
 
 
-  
+
 public class DHCP_Client {
 
 	public DHCP_Client( String ipAddress, int portIncoming, int portOutgoing, byte[] macAddress ) throws Exception{
@@ -23,7 +23,7 @@ public class DHCP_Client {
 		// the port on which the client has to operate
 		this.portOutgoing = portOutgoing;
 		this.portIncoming = portIncoming; 
-		
+
 		// the client hardware address
 		byte[] chaddr_1 = macAddress; 
 		byte[] chaddr_2 = new byte[10];
@@ -31,11 +31,11 @@ public class DHCP_Client {
 		this.MAC = array_concatenate(chaddr_1, chaddr_2);
 	}
 
-	
+
 	public DHCP_Client(String ipAddress, int port, byte[] macAddress) throws Exception {
 		this(ipAddress, port, port, macAddress);
 	}
-	
+
 	public InetAddress getIpAddress() {
 		return ipAddress;
 	}
@@ -134,8 +134,8 @@ public class DHCP_Client {
 		else {
 			this.client_connection_incoming = new DatagramSocket(this.portIncoming); 
 		}
-		
-		
+
+
 	}
 
 
@@ -170,36 +170,36 @@ public class DHCP_Client {
 	private InetAddress serverIdentifierAddress;
 	private int ipAddressLeaseTime;
 
-	
+
 
 	public InetAddress execute_DHCP() throws Exception {
 		// open a new client connection 
 		setClient_connection();
 		// send the DHCPDiscover packet -- first phase
-		
+
 		new Random().nextBytes(xid);
-		
+
 		DHCP_package DHCPDiscover = new DHCP_package(MAC);
 		DHCPDiscover.setXid(xid);
 		DHCPDiscover.set_message_type(DHCPMessageType.DHCPDISCOVER);
 		byte[] discoverOptions = {(byte) 55, (byte) 4, (byte) 1, (byte) 3, (byte) 15, (byte) 6};
 		DHCPDiscover.setOption_(discoverOptions);
-		
+
 		send_packet(DHCPDiscover.get_package());
 		System.out.println("sent packet: " + DHCPMessageType.DHCPDISCOVER.toString()  );
 		System.out.println(Arrays.toString(DHCPDiscover.get_package()));
 		//System.out.println(Arrays.toString(DHCP_discover()));
 		//send_packet(DHCP_discover());
-		
+
 		while (! is_valid_mac(DHCP_packet)){
 			this.DHCP_packet = receive_packet();
 			System.out.println("received packet: " + DHCPMessageType.DHCPOFFER.toString());
 			System.out.println(Arrays.toString(DHCP_packet.getData()));
 		}
-		
+
 		// check the received packet, which should be a DHCPOffer 
 		parse_DHCP_offer(this.DHCP_packet);
-		
+
 		// send the DHCPRequest packet
 		DHCP_package DHCPRequest = new DHCP_package(MAC); 
 		DHCPRequest.setXid(xid);
@@ -214,17 +214,17 @@ public class DHCP_Client {
 		send_packet(DHCPRequest.get_package());
 		System.out.println("sent packet: " + DHCPMessageType.DHCPREQUEST.toString()  );
 		System.out.println(Arrays.toString(DHCPRequest.get_package()));
-		
+
 		// receive the DHCPAck packet
 		DatagramPacket DHCP_ACK_packet = receive_packet();
-		
+
 		// check the received packet 
 		parse_DHCP_offer(DHCP_ACK_packet);
-		
+
 		// print it out 
 		System.out.println("received packet: " + DHCPMessageType.getMessageType(DHCPMessageNumber).toString());
 		System.out.println(Arrays.toString(DHCP_ACK_packet.getData()));
-		
+
 		// if we got a not acknowledge packet instead of an acknowledge packet, 
 		// we have to try again by sending a new discovery packet 
 		if (DHCPMessageNumber != (byte) 5){
@@ -235,125 +235,162 @@ public class DHCP_Client {
 			}
 		}
 
+
+
 		// close the client connection 
 		client_connection_outgoing.close();
 		// return the IP address 
 		return DHCPReceivedAddress;
 	}
-	
-	public void release_DHCP() throws IOException{
-		if (ipAddress == null){
-			return ;
-		}
-		DHCP_package releasePacket = new DHCP_package(MAC);
-		releasePacket.setXid(xid);
-		releasePacket.set_message_type(DHCPMessageType.DHCPRELEASE);
-		releasePacket.setSiaddr(getServerIpAddress());
-		releasePacket.setChaddr(MAC);
-		send_packet(releasePacket.get_package());
-		System.out.println("sent packet: " + DHCPMessageType.DHCPRELEASE.toString() );
-	}
 
-	void send_packet(byte[] packet) throws IOException {
-		if (this.client_connection_outgoing == null){
-			System.out.println("NO outgoing connection");
-			return ;
-		}
-		DatagramPacket sendPacket = new DatagramPacket(packet, packet.length, this.ipAddress, this.portOutgoing);
-		client_connection_outgoing.send(sendPacket);
-	}
+	public void DHCP_with_renual() throws Exception {
+		while (true) {
+			execute_DHCP();
+			while (true) {
+				Thread.sleep(getIpAddressLeaseTime()/2*1000);
 
-	DatagramPacket receive_packet() throws IOException{
-		if (this.client_connection_incoming == null){
-			return null;
-		}
-		byte[] receiveData = new byte[576];
-		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-		this.client_connection_incoming.receive(receivePacket);
-		String returnString = Arrays.toString(receivePacket.getData());
-		return receivePacket;
-	}
+				// send the DHCPRequest packet
+				DHCP_package DHCPRequest = new DHCP_package(MAC); 
+				DHCPRequest.setXid(xid);
+				DHCPRequest.setSiaddr(getServerIpAddress());
+				DHCPRequest.setChaddr(MAC);
+				DHCPRequest.set_message_type(DHCPMessageType.DHCPREQUEST);
+				byte[] option_requested_ip_one = {(byte) 50, (byte) 4};
+				byte[] option_requested_ip = array_concatenate(option_requested_ip_one, receivedIpAddress); // parameter request list, list # 55, then request subnet mask(1), router(3), domain name(15), domain name server(6)
+				byte[] option_server_ip_one = {(byte) 54, (byte) 4};
+				byte[] option_server_ip = array_concatenate(option_server_ip_one, serverIpAddress);
+				DHCPRequest.setOption_(array_concatenate(option_requested_ip, option_server_ip));
+				send_packet(DHCPRequest.get_package());
+				System.out.println("sent packet: " + DHCPMessageType.DHCPREQUEST.toString()  );
+				System.out.println(Arrays.toString(DHCPRequest.get_package()));
 
+				// receive the DHCPAck packet
+				DatagramPacket DHCP_ACK_packet = receive_packet();
 
-	private void parse_DHCP_offer(DatagramPacket offer) throws UnknownHostException{
-		if (offer == null){
-			return ;
-		}
-		byte[] DHCPData = offer.getData();
-		parse_general_data(DHCPData);
-		receivedIpAddress = Arrays.copyOfRange(DHCPData, 16, 20);
-		serverIpAddress = Arrays.copyOfRange(DHCPData, 20, 24);
-		DHCPReceivedAddress = InetAddress.getByAddress(receivedIpAddress);
-		System.out.println(DHCPReceivedAddress.getHostAddress() + " received ip address");
-		DHCPServerAddress = InetAddress.getByAddress(serverIpAddress);
-		System.out.println(DHCPServerAddress.getHostAddress() + " server ip address ");
-		if (serverIdentifierAddress != null && ! DHCPServerAddress.equals(serverIdentifierAddress)){
-			throw new UnknownHostException("The received ip adresses in the DHCP packet and in the options do not match.");
-		}
-	}
-
-	private void parse_general_data(byte[] DHCPData)
-			throws UnknownHostException {
-		byte[] options = Arrays.copyOfRange(DHCPData, 236, DHCPData.length);
-		if (! Arrays.equals(Arrays.copyOf(options, 4), MAGIC_COOKIE)){
-			return ;
-		}
-		int index = 4;
-		while (true){
-			if (options[index] == (byte) 255){
-				break;
+				// check the received packet 
+				parse_DHCP_offer(DHCP_ACK_packet);
+				
+				// if the received packet was no DHCPAck, there is no use in sending another request
+				if (DHCPMessageNumber != (byte) 5){
+					break;
+				}
 			}
-			byte optionNb = options[index];
-			int optionLength = (int) options[index + 1] & 0xFF;
-			byte[] optionContents = Arrays.copyOfRange(options, index + 2, index + 2 + optionLength);
-			parse_option(optionNb, optionLength, optionContents);
-			index+= 2 + optionLength;
 		}
 	}
 
-	private void parse_option(byte optionNb, int optionLength, byte[] optionContents) throws UnknownHostException {
-		switch (optionNb) {
-		// this option is used to convey the type of the DHCP message. its length is 1
-		// legal values are:
-		// 1    DHCPDISCOVER
-//        2     DHCPOFFER
-//        3     DHCPREQUEST
-//        4     DHCPDECLINE
-//        5     DHCPACK
-//        6     DHCPNAK
-//        7     DHCPRELEASE
-//        8     DHCPINFORM
-		case (byte) 53:
-			DHCPMessageNumber = optionContents[0];
-		break;
-		
-		// used in DHCPOFFER and DHCPREQUEST messages, to allow the
-		// client to distinguish between lease offers. 
-		case (byte) 54:
-			serverIdentifierAddress = InetAddress.getByAddress(optionContents);
-		break;
-		
-		// ip address lease time
-		// used in a client request to allow the client to request
-		// a lease time for the IP address 
-		// a server can use this option to specify the lease time it is willing to offer 
-		case (byte) 51:
-			ipAddressLeaseTime = 16777216 * (int) (optionContents[0] & 0xFF) +  65536 * (int) (optionContents[1] & 0xFF) + 256 * (int) (optionContents[2] & 0xFF) + (int) (optionContents[3] & 0xFF);
-		break;
+public void release_DHCP() throws IOException{
+	if (ipAddress == null){
+		return ;
+	}
+	DHCP_package releasePacket = new DHCP_package(MAC);
+	releasePacket.setXid(xid);
+	releasePacket.set_message_type(DHCPMessageType.DHCPRELEASE);
+	releasePacket.setSiaddr(getServerIpAddress());
+	releasePacket.setChaddr(MAC);
+	send_packet(releasePacket.get_package());
+	System.out.println("sent packet: " + DHCPMessageType.DHCPRELEASE.toString() );
+}
 
-		default:
+void send_packet(byte[] packet) throws IOException {
+	if (this.client_connection_outgoing == null){
+		System.out.println("NO outgoing connection");
+		return ;
+	}
+	DatagramPacket sendPacket = new DatagramPacket(packet, packet.length, this.ipAddress, this.portOutgoing);
+	client_connection_outgoing.send(sendPacket);
+}
+
+DatagramPacket receive_packet() throws IOException{
+	if (this.client_connection_incoming == null){
+		return null;
+	}
+	byte[] receiveData = new byte[576];
+	DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+	this.client_connection_incoming.receive(receivePacket);
+	String returnString = Arrays.toString(receivePacket.getData());
+	return receivePacket;
+}
+
+
+private void parse_DHCP_offer(DatagramPacket offer) throws UnknownHostException{
+	if (offer == null){
+		return ;
+	}
+	byte[] DHCPData = offer.getData();
+	parse_general_data(DHCPData);
+	receivedIpAddress = Arrays.copyOfRange(DHCPData, 16, 20);
+	serverIpAddress = Arrays.copyOfRange(DHCPData, 20, 24);
+	DHCPReceivedAddress = InetAddress.getByAddress(receivedIpAddress);
+	System.out.println(DHCPReceivedAddress.getHostAddress() + " received ip address");
+	DHCPServerAddress = InetAddress.getByAddress(serverIpAddress);
+	System.out.println(DHCPServerAddress.getHostAddress() + " server ip address ");
+	if (serverIdentifierAddress != null && ! DHCPServerAddress.equals(serverIdentifierAddress)){
+		throw new UnknownHostException("The received ip adresses in the DHCP packet and in the options do not match.");
+	}
+}
+
+private void parse_general_data(byte[] DHCPData)
+		throws UnknownHostException {
+	byte[] options = Arrays.copyOfRange(DHCPData, 236, DHCPData.length);
+	if (! Arrays.equals(Arrays.copyOf(options, 4), MAGIC_COOKIE)){
+		return ;
+	}
+	int index = 4;
+	while (true){
+		if (options[index] == (byte) 255){
 			break;
 		}
+		byte optionNb = options[index];
+		int optionLength = (int) options[index + 1] & 0xFF;
+		byte[] optionContents = Arrays.copyOfRange(options, index + 2, index + 2 + optionLength);
+		parse_option(optionNb, optionLength, optionContents);
+		index+= 2 + optionLength;
 	}
+}
 
-	private boolean is_valid_mac(DatagramPacket packet){
-		if (packet == null){
-			return false;
-		}
-		byte[] rawData = packet.getData();
-		byte[] possibleMac = Arrays.copyOfRange(rawData, 28, 44);
-		return Arrays.equals(MAC, possibleMac);
+private void parse_option(byte optionNb, int optionLength, byte[] optionContents) throws UnknownHostException {
+	switch (optionNb) {
+	// this option is used to convey the type of the DHCP message. its length is 1
+	// legal values are:
+	// 1    DHCPDISCOVER
+	//        2     DHCPOFFER
+	//        3     DHCPREQUEST
+	//        4     DHCPDECLINE
+	//        5     DHCPACK
+	//        6     DHCPNAK
+	//        7     DHCPRELEASE
+	//        8     DHCPINFORM
+	case (byte) 53:
+		this.DHCPMessageNumber = optionContents[0];
+	break;
+
+	// used in DHCPOFFER and DHCPREQUEST messages, to allow the
+	// client to distinguish between lease offers. 
+	case (byte) 54:
+		this.serverIdentifierAddress = InetAddress.getByAddress(optionContents);
+	break;
+
+	// ip address lease time
+	// used in a client request to allow the client to request
+	// a lease time for the IP address 
+	// a server can use this option to specify the lease time it is willing to offer 
+	case (byte) 51:
+		this.ipAddressLeaseTime = 16777216 * (int) (optionContents[0] & 0xFF) +  65536 * (int) (optionContents[1] & 0xFF) + 256 * (int) (optionContents[2] & 0xFF) + (int) (optionContents[3] & 0xFF);
+	break;
+
+	default:
+		break;
 	}
+}
+
+private boolean is_valid_mac(DatagramPacket packet){
+	if (packet == null){
+		return false;
+	}
+	byte[] rawData = packet.getData();
+	byte[] possibleMac = Arrays.copyOfRange(rawData, 28, 44);
+	return Arrays.equals(MAC, possibleMac);
+}
 
 //	public byte[] DHCP_request(){
 //		byte[] op = {(byte) 0x1};
@@ -457,23 +494,23 @@ public class DHCP_Client {
 //		return dhcp_discover_packet; 
 //	}
 //	
-	// a function to create one big array from all the given arrays
-	byte[]  array_concatenate(byte[]... array){
-		int total_length = 0;
-		for ( int i=0; i< array.length; i++){
-			total_length += array[i].length;
-		}
-		byte[] concatenated = new byte[total_length];
-		int index_concatenated = 0;
-		for (int i = 0; i< array.length; i++){
-			for (int j= 0; j < array[i].length; j++){
-				concatenated[index_concatenated] = array[i][j];
-				index_concatenated++;
-			}
-		}
-		return concatenated;
+// a function to create one big array from all the given arrays
+byte[]  array_concatenate(byte[]... array){
+	int total_length = 0;
+	for ( int i=0; i< array.length; i++){
+		total_length += array[i].length;
 	}
-	
-	
+	byte[] concatenated = new byte[total_length];
+	int index_concatenated = 0;
+	for (int i = 0; i< array.length; i++){
+		for (int j= 0; j < array[i].length; j++){
+			concatenated[index_concatenated] = array[i][j];
+			index_concatenated++;
+		}
+	}
+	return concatenated;
+}
+
+
 
 }
